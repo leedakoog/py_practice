d11640ebd9ebaff292962e5c6b1a22f42deb0b17
leedakoog <leedakoog@naver.com>
2022-07-12 16:33:44 +0900

    동명이인

--- practice.py
+++ practice.py
@@ -21,9 +21,15 @@
 # print('max is %d'%(max(a)))
 # print('maxindex is %d'%(a.index(max(a))))
 
-s = set()
-s.add(3)
-s.add(2)
-s.add(1)
-s.add(3)
-print(s)
+name = ["Tom", "Jerry", "Mike", "Tom", "Mike"]
+
+def same_name(a):
+    s = set()
+    n = len(a)
+    for x in range(n - 1):
+        for y in range(x + 1, n):
+            if a[x] == a[y]:
+                s.add(a[x])
+    return s
+print(same_name(name)[0])
+
