e642671d0c2564ce6c768706609a53f44b4fc1f3
leedakoog <leedakoog@naver.com>
2022-07-12 16:37:45 +0900

    new same name

--- practice.py
+++ practice.py
@@ -1,26 +1,3 @@
-# a = {17, 92, 18, 58, 7, 33, 42}
-
-# def Max(a):
-#     max = a[0]
-#     for x in range(len(a)):
-#         if max < a[x]:
-#             max = a[x]
-#     return max
-
-# def Max_index(a):
-#     max = a[0]
-#     max_index = 0
-#     for x in range(len(a)):
-#         if max < a[x]:
-#             max = a[x]
-#             max_index = x
-#     return max_index
-# print('max is %d'%(Max(a)))
-# print('maxindex is %d'%(Max_index(a)))
-
-# print('max is %d'%(max(a)))
-# print('maxindex is %d'%(a.index(max(a))))
-
 name = ["Tom", "Jerry", "Mike", "Tom", "Mike"]
 
 def same_name(a):
@@ -31,5 +8,5 @@ def same_name(a):
             if a[x] == a[y]:
                 s.add(a[x])
     return s
-print(same_name(name)[0])
+print(same_name(name))
 
