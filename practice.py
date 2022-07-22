1e3c2ef7824fac20ab55c007935b2108fe4dd1b6
leedakoog <leedakoog@naver.com>
2022-07-22 16:46:54 +0900

    선택정렬

--- practice.py
+++ practice.py
@@ -1,15 +1,24 @@
-def hanoi(n, a, b, c):
-    if n == 1:
-        print(a, "->", c)
-        return
-    hanoi(n - 1, a, c, b)
-    print(a, "->", c)
-    hanoi(n - 1, b, a, c)
-print("n = 1")
-hanoi(1, 1, 2, 3)
-print("n = 2")
-hanoi(2, 1, 2, 3)
-print("n = 3")
-hanoi(3, 1, 2, 3)
-print("n = 4")
-hanoi(4, 1, 2, 3)
+from unittest import result
+
+
+def find_min(arr):
+    n = len(arr)
+    min_idx = 0
+    for i in range(1, n):
+        if arr[min_idx] > arr[i]:
+            min_idx = i
+    return min_idx
+
+def sort(arr):
+    n = len(arr)
+    sort_list = []
+    while arr:
+        min_idx = find_min(arr)
+        val = arr.pop(min_idx)
+        sort_list.append(val)
+    print("arr ->", arr)
+    return sort_list
+
+
+arr = [2, 4, 5, 3 ,1]
+print(sort(arr))
