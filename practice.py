ee04013c5655fb95da1ddf3f3f2d72b2019369b8
leedakoog <leedakoog@naver.com>
2022-07-23 16:50:40 +0900

    합병정렬 내맘대로

--- practice.py
+++ practice.py
@@ -1,28 +1,33 @@
-from unittest import result
-
-
-def insert_sort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-        print(arr)
+def merge_sort(arr1, arr2):
+    ar1 = sel_sort(arr1)
+    ar2 = sel_sort(arr2)
+    list = []
+    n = 0
+    while ar1 and ar2:
+        if ar1[0] > ar2[0]:
+            list.append(ar2.pop(0))
+        else: list.append(ar1.pop(0))
+    while ar1:
+        list.append(ar1.pop(0))
+    while ar2:
+        list.append(ar2.pop(0))
+    return list
 
 def sel_sort(arr):
     n = len(arr)
     for i in range(n):
-        max_idx = i
+        min_idx = i
         for j in range(i + 1, n):
-            if arr[max_idx] < arr[j]:
-                max_idx = j
-        arr[i], arr[max_idx] = arr[max_idx], arr[i]
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
-arr = [2, 4, 5, 3 ,1]
-insert_sort(arr)
-print(arr)
+arr = [5, 3, 2, 1, 4, 6]
+n = len(arr) // 2
+arr1 = arr[:n]
+arr2 = arr[n:]
+print(merge_sort(arr1, arr2))
+
+
 
