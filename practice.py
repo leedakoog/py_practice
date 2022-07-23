b4b52128b198147a712be8738f0bce8df7c1d1b0
leedakoog <leedakoog@naver.com>
2022-07-23 16:18:44 +0900

    삽입정렬

--- practice.py
+++ practice.py
@@ -1,15 +1,28 @@
 from unittest import result
 
 
+def insert_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        print(arr)
+
 def sel_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_idx = i
+        max_idx = i
         for j in range(i + 1, n):
-            if arr[min_idx] > arr[j]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            if arr[max_idx] < arr[j]:
+                max_idx = j
+        arr[i], arr[max_idx] = arr[max_idx], arr[i]
     return arr
 
 arr = [2, 4, 5, 3 ,1]
-print(sel_sort(arr))
+insert_sort(arr)
+print(arr)
+
