3dbf9ffb4599b4afaad604884245b970fb4a8f25
leedakoog <leedakoog@naver.com>
2022-07-24 12:56:40 +0900

    merge_sortv1

--- practice.py
+++ practice.py
@@ -1,33 +1,31 @@
-def merge_sort(arr1, arr2):
-    ar1 = sel_sort(arr1)
-    ar2 = sel_sort(arr2)
-    list = []
-    n = 0
-    while ar1 and ar2:
-        if ar1[0] > ar2[0]:
-            list.append(ar2.pop(0))
-        else: list.append(ar1.pop(0))
-    while ar1:
-        list.append(ar1.pop(0))
-    while ar2:
-        list.append(ar2.pop(0))
-    return list
+from heapq import merge
+from unittest import result
 
-def sel_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i + 1, n):
-            if arr[min_idx] > arr[j]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
 
-arr = [5, 3, 2, 1, 4, 6]
-n = len(arr) // 2
-arr1 = arr[:n]
-arr2 = arr[n:]
-print(merge_sort(arr1, arr2))
+def merge_sort(a):
+    n = len(a)
+
+    if n <= 1:
+        return a
+    mid = n // 2
+    g1 = merge_sort(a[:mid])
+    g2 = merge_sort(a[mid:])
+
+    result = []
+    while g1 and g2:
+        if g1[0] < g2[0]:
+            result.append(g1.pop(0))
+        else:
+            result.append(g2.pop(0))
+    while g1:
+        result.append(g1.pop(0))
+    while g2:
+        result.append(g2.pop(0))    
+    return result
+
+
+arr = [5, 7, 3, 2, 1, 8, 4, 6]
+print(merge_sort(arr))
 
 
 
