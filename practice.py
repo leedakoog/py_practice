9d7802f90d4d2e91568ed6c61007ba3e4fce27b3
leedakoog <leedakoog@naver.com>
2022-08-06 14:14:09 +0900

    동명이인 찾기

--- practice.py
+++ practice.py
@@ -1,19 +1,20 @@
-def palindrome(arr):
-    ar = []
-
+def samename(arr):
+    d = {}
+    a = set()
     for i in arr:
-        ar.append(i)
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    for i in d:
+        if d[i] > 1:
+            a.add(i)
+    if len(a) > 0:
+        return a
+    return -1
 
-
-    while ar:
-        if len(ar) == 1:
-            return 1
-        if ar.pop(0) != ar.pop():
-            return -1
-    return 1
-        
-if palindrome("wertrew") == 1:
-    print("회문이 맞습니다.")
+arr = ["Tom", "Jerry", "Mike", "Curry", "Happy", "Tom", "Curry"]
+if samename(arr) == -1:
+    print("동명이인 없음")
 else:
-    print("회문이 아닙니다.")
-
+    print(samename(arr))
