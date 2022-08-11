596df6187054744f3f83b822ccd3c0f7c6eb84ed
leedakoog <leedakoog@naver.com>
2022-08-11 15:01:14 +0900

    최대수익

--- practice.py
+++ practice.py
@@ -1,39 +1,25 @@
-def maze_sol(maze_info, start, end):
-    qu = []
-    done = set()
+def max_profit1(stock):   # -------> 시간복잡도 n제곱
+    n = len(stock)
+    max = stock[1] - stock[0]
+    for x in range(n):
+        for y in range(x + 1, n):
+            st_max = stock[y] - stock[x]
+            if st_max > max:
+                max = st_max
+    return max
 
-    qu.append(start)
-    done.add(start)
+def max_profit2(stock):   # -------> 시간복잡도 n
+    n = len(stock)
+    max = 0
+    min = stock[0]
+    for x in range(1, n):
+        st = stock[x] - min
+        if st > max:
+            max = st
+        if stock[x] < min:
+            min = stock[x]
+    return max
 
-    while qu:
-        print(qu)
-        p = qu.pop(0)
-        print(p)
-        v = p[-1]
-        if v == end:
-            return p
-        for x in maze_info[v]:
-            if x not in done:
-                qu.append(p + x)
-                done.add(x)
-    return "?"
-
-maze_info = {
-    'a': ['e'],
-    'e': ['a', 'i'],
-    'i': ['e', 'm'],
-    'm': ['i', 'n'],
-    'n': ['m', 'j'],
-    'j': ['n', 'k', 'f'],
-    'k': ['j', 'o'],
-    'o': ['k'],
-    'f': ['j', 'g', 'b'],
-    'b': ['f', 'c'],
-    'c': ['b', 'd'],
-    'd': ['c'],
-    'g': ['f', 'h'],
-    'h': ['g', 'l'],
-    'l': ['h', 'p'],
-    'p': ['l']
-}
-print(maze_sol(maze_info, 'a', 'p'))
+stock = [10300, 9600, 9800, 8200, 7800, 8300, 9500, 9800, 10200, 9500]
+print(max_profit1(stock))
+print(max_profit2(stock))
